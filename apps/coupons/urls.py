ddb761136eb46080b974196b63ab7711cb11e27b
khxxn <kyohyun58@naver.com>
2026-09-16 21:27:20 +0900

    feat: 쿠폰 스크래치 API 구현 (#5)

--- apps/coupons/urls.py
+++ apps/coupons/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from .views import (
     CouponIssueView,
+    CouponScratchView,
 )
 
 urlpatterns = [
     # 쿠폰 발급
     path("issue/", CouponIssueView.as_view(), name="coupon-issue"),
+    # 쿠폰 긁기
+    path("<int:coupon_id>/scratch/", CouponScratchView.as_view(), name="coupon-scratch"),
 ]
--- apps/coupons/views.py
+++ apps/coupons/views.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 
 from django.db import transaction
 from rest_framework import status
@@ -9,6 +9,7 @@ from .models import (
     Coupon,
     DailyCouponCounter,
     User,
+    WinningNumber,
 )
 from .serializers import (
     CouponIssueSerializer,
@@ -62,3 +63,100 @@ class CouponIssueView(APIView):
         )
 
         return Response(CouponSerializer(coupon).data, status=status.HTTP_201_CREATED)
+
+# 쿠폰 긁기
+
+class CouponScratchView(APIView):
+
+    @transaction.atomic
+    def post(self, request, coupon_id):
+
+        try:
+            coupon = (
+                Coupon.objects
+                .select_for_update()
+                .get(
+                    coupon_id=coupon_id,
+                    deleted_at__isnull=True
+                )
+            )
+
+        except Coupon.DoesNotExist:
+            return Response(
+                {
+                    "message": "존재하지 않는 쿠폰입니다."
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        # 이미 긁은 쿠폰
+        if coupon.status != Coupon.Status.UNSCRATCHED:
+            return Response(
+                {
+                    "message": "이미 확인한 쿠폰입니다."
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # 당일 쿠폰만 스크래치 가능
+        if coupon.issued_date != date.today():
+
+            coupon.status = Coupon.Status.EXPIRED
+
+            coupon.save(
+                update_fields=[
+                    "status",
+                    "updated_at",
+                ]
+            )
+
+            data = CouponSerializer(coupon).data
+            data["message"] = "기간이 만료된 쿠폰입니다."
+
+            return Response(
+                data,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # daily_sequence가 당첨번호 DB에 존재하는지 확인
+        is_win = WinningNumber.objects.filter(
+            number=coupon.daily_sequence
+        ).exists()
+
+        coupon.scratched_at = datetime.now()
+
+        # 당첨
+        if is_win:
+            coupon.status = Coupon.Status.WIN
+
+            coupon.save(
+                update_fields=[
+                    "status",
+                    "scratched_at",
+                    "updated_at",
+                ]
+            )
+
+        
+
+            return Response(
+                CouponSerializer(coupon).data,
+                status=status.HTTP_200_OK
+            )
+
+        # 꽝
+        coupon.status = Coupon.Status.LOSE
+
+        coupon.save(
+            update_fields=[
+                "status",
+                "scratched_at",
+                "updated_at",
+            ]
+        )
+
+
+        return Response(
+            CouponSerializer(coupon).data,
+            status=status.HTTP_200_OK
+        )
